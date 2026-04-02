78bd2e1bc69bff038945b395da20fd6844e38504
atomicul <soaremihai2006@gmail.com>
2026-04-02 10:35:57 +0300

    Add dfa

---
+++ dfa.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from typing import List, Dict, Optional, Tuple
+
+
+def main():
+    initial_node, final_nodes = read_dfa()
+
+    try:
+        while True:
+            word = input()
+
+            state = initial_node.check(word)
+
+            if state is None:
+                print("Error: invalid state reached for word", word)
+
+            print("VALID" if state in final_nodes else "INVALID")
+
+    except EOFError:
+        pass
+
+
+class Node:
+    transitions: Dict[str, Node]
+
+    def __init__(self) -> None:
+        self.transitions = dict()
+
+    def check(self, word: str) -> Optional[Node]:
+        if not word:
+            return self
+
+        x, *xs = word
+        xs = "".join(xs)
+
+        n = self.transitions.get(x, None)
+
+        if n is None:
+            return None
+
+        return n.check(xs)
+
+
+def read_dfa() -> Tuple[Node, List[Node]]:
+    with open("dfa.txt", "r") as f:
+        node_count = int(f.readline())
+        nodes = [Node() for _ in range(node_count)]
+        initial_node = nodes[int(f.readline()) - 1]
+        final_nodes = [nodes[x - 1] for x in map(int, f.readline().split())]
+
+        for line in f.readlines():
+            src, letter, dest = line.split()
+            src = int(src) - 1
+            dest = int(dest) - 1
+
+            nodes[src].transitions[letter] = nodes[dest]
+
+        return (initial_node, final_nodes)
+
+
+if __name__ == "__main__":
+    main()
